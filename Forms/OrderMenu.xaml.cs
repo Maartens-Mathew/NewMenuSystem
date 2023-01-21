422adc0ec0f99557a8faee176300ed4938be213d
Mathew William Maartens <maartens.mathew@gmail.com>
2023-01-21 14:27:24 +0200

    Add project files.

---
+++ Forms/OrderMenu.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TestApp.Forms.Pages;
+using TestApp.MVVM.ViewModels;
+
+namespace TestApp.Forms
+{
+    /// <summary>
+    /// Interaction logic for OrderMenu.xaml
+    /// </summary>
+    public partial class OrderMenu : Window
+    {
+        public DrinkPage DrinkInstance;
+        public static OrderMenu OrderInstance;
+        public OrderMenu()
+        {
+            InitializeComponent();
+            OrderInstance = this;
+            currentFrame = new Frame();
+            DrinkInstance = new DrinkPage();
+        }
+
+       
+
+        private void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();
+            MainWindow.Instance.Show();
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            bdrClose.Background = Brushes.Black;
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            DataContext = new DrinksViewModel();
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            DataContext = new FoodsViewModel();
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            DataContext = new BeveragesViewModel();
+        }
+
+        private void RadioButton_Checked_3(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+            
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestApp.Forms;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance;
+        Color lightBlue = new Color();
+        Color darkBlue = new Color();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            
+            lightBlue = Color.FromRgb(10, 53, 61);  
+            darkBlue = Color.FromRgb(7, 36, 41);
+        }
+
+        
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            OrderMenu orderMenu = new OrderMenu();
+            orderMenu.Show();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Label_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.Close();    
+        }
+
+        private void Label_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            bdrClose.Background = Brushes.Black;
+        }
+
+
+    }
+}
